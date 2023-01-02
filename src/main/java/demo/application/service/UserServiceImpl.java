fdd2e521d594bcbe8090e016061bd88e29657cfc
tofetpuzo <topetemiola09@gmail.com>
2023-01-02 17:10:00 +0000

    solved the class error using basepackages

--- src/main/java/demo/application/service/UserServiceImpl.java
+++ src/main/java/demo/application/service/UserServiceImpl.java
@@ -51,7 +51,7 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public User getUser(String username) {
-        log.info("fetching username{} from databases", username);
+        log.info("fetching username{} from database", username);
         return userRepo.findByUsername(username);
     }
 
